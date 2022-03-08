3802cba84d8a3063c88af9e5ea4c67f3004b5d89
rohit-gupta09 <gupta2002rohit@gmail.com>
2022-03-08 22:03:53 +0530

    HackerRank Staircase problem

---
+++ Staircase.java
@@ -0,0 +1,31 @@
+package problemSolving;
+import java.util.*;
+public class Staircase {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+//		for(int i=0;i<n;i++)
+//		{
+//			for(int i)
+//		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j>=n-i-1)
+				{
+					System.out.print("#");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
